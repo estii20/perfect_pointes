8eda8d6de69ae365821c81a92fab5dc54134190f
estii20 <>
2024-02-05 16:56:52 +0000

    Update remove from bag view, template and url

--- bag/urls.py
+++ bag/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add_to_bag/<int:product_id>/', views.add_to_bag, name='add_to_bag'),
     path('adjust/<int:product_id>/', views.adjust_bag, name='adjust_bag'),
-    path('remove_from_bag/<int:product_id>/<int:size_id>/<int:width_id>/', views.remove_from_bag, name='remove_from_bag'),
+    path('bag/remove/<int:product_id>/<int:size_id>/<int:width_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -77,32 +77,23 @@ def adjust_bag(request, product_id):
 
 
 def remove_from_bag(request, product_id, size_id, width_id):
-    """Remove the item from the shopping bag"""
+    """ Remove a specified product from the shopping bag """
+    product = get_object_or_404(PointeShoeProduct, pk=product_id)
+    item_key = f"{size_id}_{width_id}"
+    product_id_str = str(product_id)
 
-    try:
-        product = get_object_or_404(PointeShoeProduct, pk=product_id)
-        bag = request.session.get('bag', {})
-        
-        item_key = f"{size_id}_{width_id}"
-        
-        if product_id in bag:
-            if item_key:
-                if item_key in bag[product_id]['items']:
-                    bag[product_id]['items'].pop(item_key)
-                    if not bag[product_id]['items']:
-                        bag.pop(product_id)
-                    messages.success(request, f'Removed size {size_id} {product.title} from your bag')
-                else:
-                    messages.error(request, f'Item with size {size_id} not found in your bag')
-            else:
-                bag.pop(product_id)
-                messages.success(request, f'Removed {product.title} from your bag')
+    bag = request.session.get('bag', {})
+
+    if product_id_str in bag:
+        if item_key in bag[product_id_str]['items']:
+            del bag[product_id_str]['items'][item_key]
+            if not bag[product_id_str]['items']:
+                del bag[product_id_str]
+            messages.success(request, f'Removed {product.title} from your bag')
         else:
-            messages.error(request, f'Item not found in your bag')
-        
-        request.session['bag'] = bag
-        return redirect('view_bag')
+            messages.error(request, f'{product.title} was not in your bag')
+    else:
+        messages.error(request, 'There was an error removing the item from your bag')
 
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
+    request.session['bag'] = bag
+    return redirect('view_bag')
