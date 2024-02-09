549dcdb62a5f649621f5e97ea8c41aa4059ea6d3
estii20 <>
2024-02-09 13:07:58 +0000

    Fix the PointShoeProductForm so that the price and sku save in the pointe shoe product

--- products/forms.py
+++ products/forms.py
@@ -19,11 +19,9 @@ class PointeShoeProductForm(forms.ModelForm):
     status = forms.ChoiceField(choices=PointeShoe.status_choices, label='Status', required=True) 
     feature = forms.CharField(label='Feature', widget=forms.Textarea(attrs={'rows': 4}), required=False)
 
-
     class Meta:
         model = PointeShoeProduct
         fields = ['title', 'availability', 'sku', 'image', 'image_url', 'price', 'category', 'brand', 'status', 'feature']
-
         widgets = {
             'sku': forms.HiddenInput(),
             'image': forms.HiddenInput(),
@@ -31,70 +29,35 @@ class PointeShoeProductForm(forms.ModelForm):
             'price': forms.HiddenInput(),
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        instance = kwargs.get('instance')
-        if instance:
-            self.fields['new_pointe_shoe_sku'].initial = instance.pointe_shoe.sku
-            self.fields['feature'].initial = instance.pointe_shoe.feature
-            self.fields['new_pointe_shoe_price'].initial = instance.pointe_shoe.price
-
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
-
     def save(self, commit=True):
-        pointe_shoe_instance = None  
-        if self.cleaned_data['new_pointe_shoe_name'] and self.cleaned_data['new_pointe_shoe_sku']:
-            pointe_shoe_instance = PointeShoe.objects.filter(sku=self.cleaned_data['new_pointe_shoe_sku']).first()
+            instance = super().save(commit=False)
+            
+            pointe_shoe, created = PointeShoe.objects.get_or_create(
+                sku=self.cleaned_data['new_pointe_shoe_sku'],
+                defaults={
+                    'name': self.cleaned_data['new_pointe_shoe_name'],
+                    'width': self.cleaned_data['new_pointe_shoe_width'],
+                    'shank': self.cleaned_data['new_pointe_shoe_shank'],
+                    'color': self.cleaned_data['new_pointe_shoe_color'],
+                    'price': self.cleaned_data['new_pointe_shoe_price'],
+                    'arch': self.cleaned_data['new_pointe_shoe_arch'],
+                    'ribbon': self.cleaned_data['new_pointe_shoe_ribbon'],
+                    'brand': self.cleaned_data['brand'],
+                    'category': instance.category,
+                    'status': self.cleaned_data['status'],
+                    'feature': self.cleaned_data['feature'],
+                }
+            )
+            
+            instance.title = pointe_shoe.name
+            instance.sku = pointe_shoe.sku
+            instance.price = pointe_shoe.price
+            
+            instance.pointe_shoe = pointe_shoe
 
-            if pointe_shoe_instance:
-                pointe_shoe_instance.name = self.cleaned_data['new_pointe_shoe_name']
-                pointe_shoe_instance.width = self.cleaned_data['new_pointe_shoe_width']
-                pointe_shoe_instance.shank = self.cleaned_data['new_pointe_shoe_shank']
-                pointe_shoe_instance.color = self.cleaned_data['new_pointe_shoe_color']
-                pointe_shoe_instance.price = self.cleaned_data['new_pointe_shoe_price']
-                pointe_shoe_instance.arch = self.cleaned_data['new_pointe_shoe_arch']
-                pointe_shoe_instance.ribbon = self.cleaned_data['new_pointe_shoe_ribbon']
-                pointe_shoe_instance.brand = self.cleaned_data['brand']
-                pointe_shoe_instance.category = self.instance.category
-                pointe_shoe_instance.status = self.cleaned_data['status']
-                pointe_shoe_instance.feature = self.cleaned_data['feature']
-                pointe_shoe_instance.save()
-            else:
-                pointe_shoe_instance = PointeShoe.objects.create(
-                    name=self.cleaned_data['new_pointe_shoe_name'],
-                    sku=self.cleaned_data['new_pointe_shoe_sku'],
-                    width=self.cleaned_data['new_pointe_shoe_width'],
-                    shank=self.cleaned_data['new_pointe_shoe_shank'],
-                    color=self.cleaned_data['new_pointe_shoe_color'],
-                    price=self.cleaned_data['new_pointe_shoe_price'],
-                    arch=self.cleaned_data['new_pointe_shoe_arch'],
-                    ribbon=self.cleaned_data['new_pointe_shoe_ribbon'],
-                    brand=self.cleaned_data['brand'],
-                    category=self.instance.category,
-                    status=self.cleaned_data['status'],
-                    feature=self.cleaned_data['feature'],
-                )
-                pointe_shoe_instance.available_sizes.set(self.cleaned_data['available_sizes'])
-                pointe_shoe_instance.available_widths.set(self.cleaned_data['available_widths'])
-
-        else:
-            pointe_shoe_instance = self.instance.pointe_shoe
-
-            pointe_shoe_instance.name = self.cleaned_data['new_pointe_shoe_name']
-            pointe_shoe_instance.width = self.cleaned_data['new_pointe_shoe_width']
-            pointe_shoe_instance.shank = self.cleaned_data['new_pointe_shoe_shank']
-            pointe_shoe_instance.color = self.cleaned_data['new_pointe_shoe_color']
-            pointe_shoe_instance.price = self.cleaned_data['new_pointe_shoe_price']
-            pointe_shoe_instance.arch = self.cleaned_data['new_pointe_shoe_arch']
-            pointe_shoe_instance.ribbon = self.cleaned_data['new_pointe_shoe_ribbon']
-            pointe_shoe_instance.brand = self.cleaned_data['brand']
-            pointe_shoe_instance.category = self.instance.category
-            pointe_shoe_instance.status = self.cleaned_data['status']
-            pointe_shoe_instance.feature = self.cleaned_data['feature']
-            pointe_shoe_instance.save()
-
-        self.instance.pointe_shoe = pointe_shoe_instance
-
-        return super().save(commit)
-
+            if commit:
+                instance.save()
+                instance.pointe_shoe.available_sizes.set(self.cleaned_data['available_sizes'])
+                instance.pointe_shoe.available_widths.set(self.cleaned_data['available_widths'])
+                self.save_m2m()
+            return instance
