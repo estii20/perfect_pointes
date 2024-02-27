5d0ccf4096be74f8d25cd6d18a6788799cf43cea
estii20 spe <estellespecht@hotmail.co.uk>
2024-02-27 11:26:10 +0000

    Add environmental variables to GitPod and update settings.py

--- perfect_pointes/settings.py
+++ perfect_pointes/settings.py
@@ -22,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key_here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['perfect-pointes-d5136a797ac1.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [
+    'perfect-pointes-d5136a797ac1.herokuapp.com',
+    '8000-estii20-perfectpointes-rh7g27vd73f.ws-eu108.gitpod.io',
+    'localhost'
+]
+
 
 # Application definition
 
@@ -175,6 +180,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
@@ -210,4 +217,5 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'perfect_pointes@example.com'
