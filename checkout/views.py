fc62e2a694bf78e1872868fe4ec97bada973e749
estii20 spe <estellespecht@hotmail.co.uk>
2024-03-02 12:48:52 +0000

    Fix checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -53,8 +53,8 @@ def checkout(request):
             'postcode': request.POST['postcode'],
             'town_or_city': request.POST['town_or_city'],
             'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST.get('street_address2', ''),
-            'county': request.POST.get('county', ''),
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
         }
 
         order_form = OrderForm(form_data)
@@ -63,25 +63,8 @@ def checkout(request):
             pid = request.POST.get('client_secret').split('_secret')[0]
             order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
-
-            # Check if user is authenticated and save profile data
-            if request.user.is_authenticated and request.POST.get('save-info') == 'on':
-                user = request.user
-                profile_data = {
-                    'default_phone_number': form_data['phone_number'],
-                    'default_country': form_data['country'],
-                    'default_postcode': form_data['postcode'],
-                    'default_town_or_city': form_data['town_or_city'],
-                    'default_street_address1': form_data['street_address1'],
-                    'default_street_address2': form_data['street_address2'],
-                    'default_county': form_data['county'],
-                }
-                profile_data['user_id'] = user.id
-                profile, created = UserProfile.objects.get_or_create(user=user, defaults=profile_data)
-
             order.save()
 
-            # Create order line items
             for product_id, item_data in bag.items():
                 product = get_object_or_404(PointeShoeProduct, pk=product_id)
                 if isinstance(item_data, int):
@@ -100,11 +83,14 @@ def checkout(request):
                         )
                         order_line_item.save()
 
-            # Set session variable for saving info
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            messages.error(request, 'There was an error with your form. Please double check your information.')
+            messages.error(
+                request,
+                'There was an error with your form. '
+                'Please double check your information.'
+            )
     else:
         bag = request.session.get('bag', {})
         if not bag:
@@ -120,13 +106,10 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # If user is authenticated, pre-populate form with profile data
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
                 order_form = OrderForm(initial={
-                    'full_name': profile.user.get_full_name(),
-                    'email': profile.user.email,
                     'phone_number': profile.default_phone_number,
                     'country': profile.default_country,
                     'postcode': profile.default_postcode,
@@ -141,10 +124,22 @@ def checkout(request):
             order_form = OrderForm()
 
         if not stripe_public_key:
-            messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
+            messages.warning(
+                request,
+                'Stripe public key is missing. '
+                'Did you forget to set it in your environment?'
+            )
 
-        available_brands = PointeShoeBrand.objects.filter(pointeshoe__pointeshoeproduct__availability=True).distinct()
-        available_categories = Category.objects.filter(pointeshoe__pointeshoeproduct__availability=True).distinct()
+        available_brands = (
+            PointeShoeBrand.objects
+            .filter(pointeshoe__pointeshoeproduct__availability=True)
+            .distinct()
+        )
+        available_categories = (
+            Category.objects
+            .filter(pointeshoe__pointeshoeproduct__availability=True)
+            .distinct()
+        )
 
         context = {
             'order_form': order_form,
